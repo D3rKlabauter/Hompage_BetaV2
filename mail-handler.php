5dba937fe9fbf721555038e140dad6dc1f882f59
D3rKlabauter <68585996+D3rKlabauter@users.noreply.github.com>
2022-05-25 12:25:47 +0200

    Update mail-handler.php

--- mail-handler.php
+++ mail-handler.php
@@ -43,7 +43,7 @@ if( empty($errors))
 
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<!DOCTYPE html> 
 <html>
 <head>
 	<title>Contact form handler</title>
